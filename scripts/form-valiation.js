869008a1e28477297452aa8e74eedbb0a323e2d0
Alex Muñoz <astroboy.reloaded@gmail.com>
2023-02-02 09:25:06 -0600

    Fix ESLint errors

--- scripts/form-valiation.js
+++ scripts/form-valiation.js
@@ -3,13 +3,13 @@ const form = document.forms[0];
 function validateForm(event) {
   event.preventDefault();
 
-  const email = document.querySelector("#email").value;
+  const email = document.querySelector('#email').value;
   if (email.toLowerCase() !== email) {
-    const errorMsg = document.querySelector("#errorMsg");
-    errorMsg.textContent = "Email must be in lower case!";
+    const errorMsg = document.querySelector('#errorMsg');
+    errorMsg.textContent = 'Email must be in lower case!';
     return;
   }
   form.submit();
 }
 
-form.addEventListener("submit", validateForm);
+form.addEventListener('submit', validateForm);
