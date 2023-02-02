fd2c54abad296a6a664127ddf130cc27c9423a6d
Muhammed Berisso <muhammedb8826@gmail.com>
2023-02-02 18:03:02 +0300

    Add form-validation.js

---
+++ scripts/form-valiation.js
@@ -0,0 +1,15 @@
+const form = document.forms[0];
+
+function validateForm(event) {
+  event.preventDefault();
+
+  const email = document.querySelector("#email").value;
+  if (email.toLowerCase() !== email) {
+    const errorMsg = document.querySelector("#errorMsg");
+    errorMsg.textContent = "Email must be in lower case!";
+    return;
+  }
+  form.submit();
+}
+
+form.addEventListener("submit", validateForm);
