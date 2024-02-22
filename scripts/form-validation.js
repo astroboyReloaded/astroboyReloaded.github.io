6448cdb078341eb2443711251825da4ccd8750b3
AstroboyReloaded <astroboy.reloaded@gmail.com>
2024-02-21 21:56:58 -0600

    Update close menu after 'sifht + tab' (mobile)

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -1,6 +1,9 @@
 const [Name, Email, Message] = document.querySelectorAll('.formInput'),
   [nameErr, emailErr, messageErr] = document.querySelectorAll('.errMsgContainer'),
   SubmitBtn = document.getElementById('submit');
+const AllInputs = [Name, Email, Message],
+  Errors = [nameErr, emailErr, messageErr],
+  showErr = 'showErr';
 Name.addEventListener('input', function() {isNameValid(this.value)});
 Email.addEventListener('input', function() {isEmailValid(this.value)});
 Message.addEventListener('input', function() {isMessageValid(this.value)});
@@ -8,16 +11,15 @@ Message.addEventListener('input', function() {isMessageValid(this.value)});
 let ValidName = false,
   ValidEmail = false,
   ValidMsg = false;
-
 export function isNameValid(name) {
   const Valid = /[a-zA-z]+/.test(name),
     ClassList = nameErr.classList;
 
   if (!Valid) {
-    !ClassList.contains('showErr') && ClassList.add('showErr');
+    !ClassList.contains(show) && ClassList.add(show);
     nameErr.textContent = 'Name must be at least one Alphabetic character (a-zA-Z).';
   } else {
-    ClassList.contains('showErr') && ClassList.remove('showErr');
+    ClassList.contains(show) && ClassList.remove(show);
     nameErr.textContent = '';
   }
   ValidName = Valid;
@@ -32,10 +34,10 @@ export function isEmailValid(email) {
 
   const msg = 'Email must be in';
   if (Valid) {
-    ClassList.contains('showErr') && ClassList.remove('showErr');
+    ClassList.contains(show) && ClassList.remove(show);
     emailErr.textContent = '';
   } else {
-    !ClassList.contains('showErr') && ClassList.add('showErr');
+    !ClassList.contains(show) && ClassList.add(show);
     if (!isLower) {
       emailErr.textContent = msg + ' lower case';
     }
@@ -45,22 +47,22 @@ export function isEmailValid(email) {
   }
   ValidEmail = Valid;
   helpValidate(Email, Valid);
-}
+};
 
 export function isMessageValid(msg) {
     const Valid = msg.length >= 4 && msg.length <= 500,
       ClassList = messageErr.classList;
 
   if (!Valid) {
-    !ClassList.contains('showErr') && ClassList.add('showErr');
+    !ClassList.contains(show) && ClassList.add(show);
     messageErr.textContent = 'Message must be between 4 and 500 characters.';
   } else {
-    ClassList.contains('showErr') && ClassList.remove('showErr');
+    ClassList.contains(show) && ClassList.remove(show);
     messageErr.textContent = '';
   }
   ValidMsg = Valid;
   helpValidate(Message, Valid);
-}
+};
 
 function helpValidate(input, valid) {
   const Class = input.classList;
@@ -71,16 +73,14 @@ function helpValidate(input, valid) {
     Class.add('green');
   }
   validateForm();
-}
+};
+
 function validateForm() {
   const ValidForm = ValidName && ValidEmail && ValidMsg;
   SubmitBtn.disabled = !ValidForm;
 }
 
 // Allow interaction
-const AllInputs = [Name, Email, Message],
-  Errors = [nameErr, emailErr, messageErr],
-  hide = 'hideError';
 AllInputs.forEach(input => {
   input.addEventListener('blur', () => showAllErrors(input));
   input.addEventListener('focus', () => hideOtherErrors(input));
@@ -88,11 +88,16 @@ AllInputs.forEach(input => {
 
 function showAllErrors(input) {
   input.classList.remove('green');
+  Errors.forEach(err => {
+    err.id === `${input.id}Err` && err.classList.remove(show);
+  })
   const allEmpty = AllInputs.map(input => input.value === '');
   if (allEmpty.every(value => value === true)) {
     AllInputs.forEach(input => {
-      input.classList.remove('green');
-      input.classList.remove('red');
+      input.classList.remove('green', 'red');
+      Errors.forEach(err => {
+        err.classList.remove(show);
+      })
     })
     Errors.forEach(error => {
       error.classList.add(hide)
@@ -103,11 +108,16 @@ function showAllErrors(input) {
     error.classList.remove(hide);
   })
   }
-}
+};
+
 function hideOtherErrors(input) {
   Errors.forEach(error => {
     !error.classList.contains(hide) && error.classList.add(hide);
+    if (!error.value && error.id === `${input.id}Err`) {
+      error.classList.add(hide)
+    }
   });
+
   const verified = () => input.classList.add('green');
   switch (input.id) {
     case 'name':
@@ -125,6 +135,4 @@ function hideOtherErrors(input) {
     default:
       break;
   }
-}
-
-document.addEventListener('keypress', () => console.log(document.activeElement))
+};
